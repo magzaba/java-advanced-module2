3e2679ab7ce1f899994f259858268f9c26c46fd8
Magdalena <Magdalena_Zaba@epam.com>
2024-11-19 13:43:46 +0100

    Task 3 - Where's your bus dude?

---
+++ src/main/java/com/epam/backend/core/multithreading/bus/MessageBus.java
@@ -0,0 +1,28 @@
+package com.epam.backend.core.multithreading.bus;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+public class MessageBus {
+    private final Map<String, Queue<Message>> queues = new HashMap<>();
+
+    public synchronized void postMessage(Message message) {
+        String topic = message.getTopic();
+        queues.putIfAbsent(topic, new LinkedList<>());
+        queues.get(topic).offer(message);
+        notifyAll();
+    }
+
+    public synchronized Message consumeMessage(String topic) {
+        while (!queues.containsKey(topic) || queues.get(topic).isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        return queues.get(topic).poll();
+    }
+}
